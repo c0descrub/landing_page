fdd4e7d2defa56ce5ba2a515faebfd13e7dfd241
c0descrub <jordanjames1095@googlemail.com>
2022-09-28 17:52:33 +0100

    more meta data added

--- pages/_app.js
+++ pages/_app.js
@@ -8,14 +8,25 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <Head>
         <title>Jordan Smith</title>
-        <meta name="description" content="undefined" key="description" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+
+        <meta
+          name="description"
+          content="frontend web developer javascript react.js next.js"
+          key="description"
+        />
 
         <meta property="og:url" content="https://jordansmith.cc/" key="url" />
         <meta property="og:type" content="website" key="type" />
-        <meta property="og:title" content="Jordan Smith" key="title" />
+        <meta
+          property="og:title"
+          content="Jordan Smith - Frontend Developer"
+          key="title"
+        />
         <meta
           property="og:description"
-          content="front-end web developer landing page with latest projects and github links"
+          content="Frontend Developer working in React.js"
           key="description"
         />
         <meta property="og:image" content="" key="image" />
@@ -35,10 +46,14 @@ function MyApp({ Component, pageProps }) {
           content="https://jordansmith.cc/"
           key="twitter-url"
         />
-        <meta name="twitter:title" content="Jordan Smith" key="twitter-title" />
+        <meta
+          name="twitter:title"
+          content="Jordan Smith - Frontend Developer"
+          key="twitter-title"
+        />
         <meta
           name="twitter:description"
-          content="front-end web developer landing page with latest projects and github links"
+          content="Frontend Developer working in React.js"
           key="twitter-description"
         />
         <meta name="twitter:image" content="" key="twitter-content" />
