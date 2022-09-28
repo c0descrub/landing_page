44a60a1ec4cc2d2548ed81e1cfe638ff512a67ab
c0descrub <jordanjames1095@googlemail.com>
2022-09-28 09:29:01 +0100

    All content added. Moving onto styling.

---
+++ components/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer>
+      <hr></hr>
+      <ul>
+        <li>
+          <a>Github</a>
+        </li>
+        <li>
+          <a>CV</a>
+        </li>
+        <li>
+          <a>info@jordansmith.cc</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
