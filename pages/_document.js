2b15cc8e8ee7ffa73de0307efe240b2371db7d92
c0descrub <jordanjames1095@googlemail.com>
2022-10-04 14:12:23 +0100

    changed google font param from swap to optional. Removed CSS rule for font-display

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=optional"
           rel="stylesheet"
         />
       </Head>
