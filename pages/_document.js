ed5d006fa6ee7f95a8782171535e95ba2f5b9fd0
c0descrub <jordanjames1095@googlemail.com>
2022-10-04 15:48:54 +0100

    changed from optional to fallback to prevent fonts from not loading.

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-<<<<<<< HEAD
-=======
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
->>>>>>> 41ebe4b97a8daf405053ce3563a196b7dee029e7
         <link
-          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=optional"
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=fallback"
           rel="stylesheet"
         />
       </Head>
